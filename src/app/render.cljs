0a7562a8b270165717b5547f8acccdb6b1d1cda0
ChenYong <jiyinyiyong@dingtalk.com>
2017-11-27 18:45:31 +0800

    upgrade deps

--- src/app/render.cljs
+++ src/app/render.cljs
@@ -6,10 +6,7 @@
             [app.schema :as schema]))
 
 (def base-info
-  {:title "EDN Formatter",
-   :icon "http://cdn.tiye.me/logo/mvc-works.png",
-   :ssr nil,
-   :inline-html nil})
+  {:title "EDN Formatter", :icon "http://cdn.tiye.me/logo/mvc-works.png", :ssr nil})
 
 (defn dev-page []
   (make-page
@@ -17,26 +14,21 @@
    (merge
     base-info
     {:styles ["http://localhost:8100/main.css"],
-     :scripts ["/main.js" "/browser/lib.js" "/browser/main.js"]})))
+     :scripts ["/browser/lib.js" "/browser/main.js"]})))
 
 (def previews? (= "preview" js/process.env.prod))
 
 (defn prod-page []
   (let [html-content (make-string (comp-container schema/store))
-        webpack-info (.parse js/JSON (slurp "dist/webpack-manifest.json"))
-        cljs-info (.parse js/JSON (slurp "dist/cljs-manifest.json"))
+        assets (.parse js/JSON (slurp "dist/assets.edn"))
         cdn (if previews? "" "http://cdn.tiye.me/edn-formatter/")
         prefix-cdn (fn [x] (str cdn x))]
     (make-page
      html-content
      (merge
       base-info
-      {:styles ["http://cdn.tiye.me/favored-fonts/main.css"
-                (prefix-cdn (aget webpack-info "main.css"))],
-       :scripts (map
-                 prefix-cdn
-                 [(-> cljs-info (aget 0) (aget "js-name"))
-                  (-> cljs-info (aget 1) (aget "js-name"))]),
+      {:styles ["http://cdn.tiye.me/favored-fonts/main.css"],
+       :scripts (map #(-> % :output-name prefix-cdn) assets),
        :ssr "respo-ssr"}))))
 
 (defn main! []
